699fe392bae94e2ebf2d7b527ed949f280b0a0a1
Pieter Rees <pieter@pieterrees.nl>
2016-11-27 23:27:48 +0100

    Added some index navigation and aroma rating (min-max not working)

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function () {
+		return Ember.RSVP.hash({
+			aromas: this.store.findAll('aroma'),
+			recipes: this.store.findAll('recipe')
+		});
+	}
 });
