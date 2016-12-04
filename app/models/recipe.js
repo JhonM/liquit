fa76d66943904ad849e466777f63a637d823df01
Pieter Rees <pieter@pieterrees.nl>
2016-12-04 22:51:55 +0100

    Reverted aroma component

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -2,6 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  dateMade: DS.attr(),
+  brewDate: DS.attr('number'),
+  description: DS.attr('string'),
+  steepTime: DS.attr('string'),
+  brewPlan: DS.attr('string'),
   aromas: DS.hasMany('aroma', {async: true})
 });
