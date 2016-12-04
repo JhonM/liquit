b9575a6ae09a0f094719d5c4ef3cc15fec867ca9
Pieter Rees <pieter@pieterrees.nl>
2016-12-04 23:29:51 +0100

    I have to fix it  with the example on: https://github.com/Pieter-Rees/dragndrop-recipe-book/blob/master/app/routes/recipes/recipe.js

--- app/routes/recipes/new.js
+++ app/routes/recipes/new.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 import SaveModelMixin from 'liquit/mixins/recipes/save-model-mixin';
 
 export default Ember.Route.extend(SaveModelMixin, {
-	
+
 	model: function () {
-		return this.store.createRecord('recipe');
+		return this.store.createRecord('recipe', {
+			Test: '1',
+			TestX: 'Lorem ipsum'
+		});
 	}
 
 });
