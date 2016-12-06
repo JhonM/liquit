1e13ee2003737470d7f824978cdc0cbb1aba9b1d
Pieter Rees <pieter@pieterrees.nl>
2016-12-06 21:24:23 +0100

    Added new component and rsvp hash for finding aroma's and adding recipes at once

--- app/routes/recipes/new.js
+++ app/routes/recipes/new.js
@@ -2,12 +2,19 @@ import Ember from 'ember';
 import SaveModelMixin from 'liquit/mixins/recipes/save-model-mixin';
 
 export default Ember.Route.extend(SaveModelMixin, {
-
 	model: function () {
-		return this.store.createRecord('recipe', {
-			Test: '1',
-			TestX: 'Lorem ipsum'
+		//return this.store.createRecord('recipe', {
+		//	Test: '1',
+		//	TestX: 'Lorem ipsum'
+		//});
+		return Ember.RSVP.hash({
+			newRecipe: this.store.createRecord('recipe', {
+				title: '',
+				description: ''
+			}),
+			aromas: this.store.findAll('aroma')
 		});
 	}
+
 
 });
