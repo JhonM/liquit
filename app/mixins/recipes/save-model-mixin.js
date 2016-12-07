7fc2b6d440c4b2ac5b94acc90e999dc6ed1c3eca
Pieter Rees <pieter@pieterrees.nl>
2016-12-07 22:50:46 +0100

    Add fix

--- app/mixins/recipes/save-model-mixin.js
+++ app/mixins/recipes/save-model-mixin.js
@@ -4,7 +4,7 @@ export default Ember.Mixin.create({
   actions: {
     save: function() {
       var route = this;
-      this.currentModel.newRecipe.save().then(function() {
+      this.newRecipe.save().then(function() {
       }, function() {
         console.log('Failed to save the model');
       });
