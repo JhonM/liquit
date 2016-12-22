f17ba63942099ddb37800b6dff9084d80ff38ae3
Pieter Rees <pieter@pieterrees.nl>
2016-12-22 20:04:34 +0100

    MEh

--- app/models/ingredient.js
+++ app/models/ingredient.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	 aromas: DS.hasMany( 'aroma', { async: true } ),
+	 aromas: DS.belongsTo( 'aroma', { async: true } ),
 	 amount: DS.attr('number')
 });
