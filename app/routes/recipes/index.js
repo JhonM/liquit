7ca2bb758c9d01d9bb012338ce16ac4f79801fb8
stofstik <verstegen.daan@gmail.com>
2016-12-28 14:26:07 +0100

    implemented delete multiple

--- app/routes/recipes/index.js
+++ app/routes/recipes/index.js
@@ -6,13 +6,27 @@ export default Ember.Route.extend({
       if(confirm('Are you sure?')) {
         model.destroyRecord();
       }
+    },
+    removeSelected: function() {
+      // Get all selected checkboxes
+      const selected = Ember.$("input:checked");
+      if(selected.length < 1) return;
+      if(confirm('Are you sure you want to delete ' + selected.length + ' items?')) {
+        // Remove selected
+        selected.map((index, cb) => {
+          const id = cb.id.substring('recipe-'.length, cb.id.length)
+          this.store.find('recipe', id).then((recipe) => {
+            recipe.destroyRecord();
+          })
+        })
+      }
     }
   },
   model: function() {
-	  return Ember.RSVP.hash({
-  		aromas: this.store.findAll('aroma'),
-  		recipes: this.store.findAll('recipe')
-  	});
+    return Ember.RSVP.hash({
+      aromas: this.store.findAll('aroma'),
+      recipes: this.store.findAll('recipe')
+    });
 
   }
 });
