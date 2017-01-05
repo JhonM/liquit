dd2acb63fad3cf5026bff7e9846516ea3d8f07bb
Pieter Rees <pieter@pieterrees.nl>
2017-01-05 21:40:52 +0100

    Added nice navigation animations

--- app/transitions.js
+++ app/transitions.js
@@ -1,10 +1,40 @@
 import Ember from 'ember';
 
-export default function(){
-  this.transition(
-    this.fromRoute('index'),
-    this.toRoute('recipes.index'),
-    this.use('toLeft'),
-    this.reverse('toRight')
-  );
+export default function() {
+
+    this.transition(
+        this.toRoute('recipes'),
+        this.use('toRight'),
+        this.reverse('toLeft')
+    );
+    this.transition(
+        this.toRoute('aromas'),
+        this.use('toLeft'),
+        this.reverse('toRight')
+    );
+    this.transition(
+        this.fromRoute('index'),
+        this.toRoute('recipes'),
+        this.use('toLeft'),
+        this.reverse('toRight')
+    );
+    this.transition(
+        this.fromRoute('index'),
+        this.toRoute('aromas'),
+        this.use('toLeft'),
+        this.reverse('toRight')
+    );
+
+    // Not working
+    this.transition(
+        this.toRoute('recipes.new'),
+        this.use('crossFade'),
+        this.reverse('crossFade')
+    );
+    this.transition(
+        this.toRoute('aromas.new'),
+        this.use('crossFade'),
+        this.reverse('crossFade')
+    );
+    // Not working end
 };
